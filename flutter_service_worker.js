ca37247bdd328e8c53f421ff73a148b18eb24d02
Your Name <your-email@example.com>
2025-07-15 19:06:59 -0400

    Built web
    Branch: main
    Commit: fa9af6d33e0e0059015346418d85b397a07662a3 (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a91e0acb85b9d7804e587aee8bdf02f0
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "5dde74015bfb306ddc8bed941609cfdb",
+"flutter_bootstrap.js": "b057d8766267cdf81f07dc7d01690990",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
